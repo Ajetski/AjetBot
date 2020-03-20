6f618cd281f75715b333d52a9712289985e56093
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2020-03-19 20:55:06 -0400

    add comments and logging

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,35 @@
-node_pid=`pidof node`
+######################################################################
+# Author: Adam Jeniski
+# Project: AjetBot
+# File: /deploy/deploy.sh
+# 
+# Deploys the latest version of https://github.com/Ajetski/AjetBot
+#
+######################################################################
 
+echo "============================================"
+echo "Running deployment script"
+echo "============================================"
+echo ""
+echo "============================================"
+echo "Killing old node process"
+echo ""
+
+node_pid=`pidof node`
 kill $node_pid
 
-cd AjetBot
+echo "============================================"
+echo "Updating source"
+echo "============================================"
+echo ""
 
+cd AjetBot
 git pull
+
+echo "============================================"
+echo "Re-deploying"
+echo "============================================"
+echo ""
+echo "done..."
 
 node src/index.js &
