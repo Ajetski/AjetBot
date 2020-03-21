a8d0197fc7b77b848df6850cc1d6ed37254f9a48
Caleb Garrick <caleb.garrick@gmail.com>
2020-03-21 15:48:39 -0400

    add case insensitivity and keywords for tarkov maps

--- src/index.js
+++ src/index.js
@@ -27,16 +27,18 @@ client.once('ready', () => {
 
 client.on('message', msg => {
 
+	const cont = msg.content.toLowerCase();
+
 	/*
 	============
 		TEST
 	============
 	*/
-	if(msg.content.match(/^!ping$/)){
+	if(cont.match(/^!ping$/)){
 		msg.reply('pong');
 	}
 
-	if(msg.content.match(/^!update$/)){
+	if(cont.match(/^!update$/)){
 		msg.reply('Updating...');
 		exec('../deploy/deploy.sh');
 	}
@@ -47,27 +49,27 @@ client.on('message', msg => {
 	============
 	*/
 
-	if(msg.content.match(/^!ty$/)) {
+	if(cont.match(/^!ty$/)) {
 		msg.channel.send('sup <@267848388735664128>');
 	}
 
-	if(msg.content.match(/^!cris$/)) {
+	if(cont.match(/^!cris$/)) {
 		msg.channel.send('sup <@94188288436342784>');
 	}
 
-	if(msg.content.match(/^!john$/)) {
+	if(cont.match(/^!john$/)) {
 		msg.channel.send('sup <@340249267769835520>');
 	}
 
-	if(msg.content.match(/^!caleb$/) || msg.content.match(/^!kleb$/) || msg.content.match(/^!daddy$/)) {
+	if(cont.match(/^!caleb$/) || cont.match(/^!kleb$/) || cont.match(/^!daddy$/)) {
 		msg.channel.send('sup <@215636797189914625>');
 	}
 
-	if(msg.content.match(/^!nj$/)) {
+	if(cont.match(/^!nj$/)) {
 		msg.channel.send('sup <@139109067527290880>');
 	}
 
-	if(msg.content.match(/^!getshiton$/)){
+	if(cont.match(/^!getshiton\s$/)){
 		msg.channel.send('░░░░░░░░░░░█▀<@' + msg.author.id + '>█░░░░\n░░░░░░▄▀▀▀▀░░░░░█▄▄░░░░\n░░░░░░█░█░░░░░░░░░░▐░░░\n░░░░░░▐▐░░░░░░░░░▄░▐░░░\n░░░░░░█░░░░░░░░▄▀▀░▐░░░\n░░░░▄▀░░░░░░░░▐░▄▄▀░░░░\n░░▄▀░░░▐░░░░░█▄▀░▐░░░░░\n░░█░░░▐░░░░░░░░▄░█░░░░░\n░░░█▄░░▀▄░░░░▄▀▐░█░░░░░\n░░░█▐▀▀▀░▀▀▀▀░░▐░█░░░░░\n░░▐█▐▄░░▀░░░░░░▐░█▄▄░░░\n░░░▀▀░  <@152850062228193280>  ░░░▐▄▄▄▀░░░\n░░░░░░░░░░░░░░░░░░░░░░░');
 	}
 	/*
@@ -76,122 +78,128 @@ client.on('message', msg => {
 	============
 	*/
 
-	if(msg.content.match(/^!pogchamp$/) || msg.content.match(/^!poggers$/)){
+	if(cont.match(/^!pogchamp$/) || cont.match(/^!poggers$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/pogChamp.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!pogchump$/)){
+	if(cont.match(/^!pogchump$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/pogChump.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!bigbrain$/)){
+	if(cont.match(/^!bigbrain$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/bigBrain.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!feelsbadman$/)){
+	if(cont.match(/^!feelsbadman$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/feelBad.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!ree$/)){
+	if(cont.match(/^!ree$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/ree.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!trump$/) || msg.content.match(/^!drump$/)){
+	if(cont.match(/^!trump$/) || cont.match(/^!drump$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/drump.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!feelscoolman$/)){
+	if(cont.match(/^!feelscoolman$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/feelCool.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!sweaty$/)){
+	if(cont.match(/^!sweaty$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/feelScary.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!kek$/)){
+	if(cont.match(/^!kek$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/kek.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!kermit$/)){
+	if(cont.match(/^!kermit$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/kermitHM.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!nani$/)){
+	if(cont.match(/^!nani$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/nani.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!fuckyou$/)){
+	if(cont.match(/^!fuckyou$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/rickFlip.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!ohmydog$/)){
+	if(cont.match(/^!ohmydog$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/arf.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!coolnoah$/)){
+	if(cont.match(/^!coolnoah$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/coolDoge.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!dawae$/) || msg.content.match(/^!penguin$/)){
+	if(cont.match(/^!dawae$/) || cont.match(/^!penguin$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/echidna.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!ohmygod$/) || msg.content.match(/^!slayer$/) || msg.content.match(/^!gnome$/) || msg.content.match(/^!god$/)){
+	if(cont.match(/^!ohmygod$/) || cont.match(/^!slayer$/) || cont.match(/^!gnome$/) || cont.match(/^!god$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/rsGnome.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!geckw$/)){
+	if(cont.match(/^!geckw$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/geckw.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!moon2l$/)){
+	if(cont.match(/^!moon2l$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/moon2l.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!pregigi$/)){
+	if(cont.match(/^!pregigi$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/pregigi.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!pregario$/)){
+	if(cont.match(/^!pregario$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/pregario.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!moon2boo$/)){
+	if(cont.match(/^!moon2boo$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/moon2Boo.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!poggy$/)){
+	if(cont.match(/^!poggy$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/moon2poggy.png');
 		msg.channel.send(attachment);
 	}
 
-	if(msg.content.match(/^!illuminati$/)){
+	if(cont.match(/^!illuminati$/)){
 		const attachment = new Discord.MessageAttachment('./src/media/illuminati.png');
 		msg.channel.send(attachment);
 	}
+
+	/*
+	============
+	Random Memes
+	============
+	*/
 	
-	if(msg.content.match(/^!wojack$/)){
+	if(cont.match(/^!wojack$/)){
 
 		const wojacksPath = path.join(process.cwd(), 'src/media/Wojacks');
 		const files = fs.readdirSync(wojacksPath);
@@ -199,17 +207,58 @@ client.on('message', msg => {
 		const attachment = new Discord.MessageAttachment(path.join(wojacksPath, chosenFile));
 		msg.channel.send(attachment);
 	}
-	else if(msg.content.match(/^!pepe$/)){
+	else if(cont.match(/^!pepe$/)){
 		const files = fs.readdirSync(path.join(__dirname,'media/Pepes'));
 		const chosenFile = files[Math.floor(Math.random()*files.length)];
 		const attachment = new Discord.MessageAttachment(path.join(__dirname, 'media/Pepes/', chosenFile));
 		msg.channel.send(attachment);
 	}
-	else if(msg.content.match(/^!feelinglucky$/)){
+	else if(cont.match(/^!feelinglucky$/)){
 		const files = fs.readdirSync(path.join(__dirname,'media/Asciiart'));
 		const chosenFile = files[Math.floor(Math.random()*files.length)];
 		const data = fs.readFileSync(path.join(__dirname, 'media/Asciiart/', chosenFile));
 		msg.channel.send(data.toString());
+	}
+
+	/*
+	===========
+	TARKOV MAPS
+	===========
+	*/
+
+	if(cont.match(/^!customs$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/customs.png');
+		msg.channel.send(attachment);
+	}
+
+	if(cont.match(/^!factory$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/factory.png');
+		msg.channel.send(attachment);
+	}
+
+	if(cont.match(/^!interchange$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/interchange.png');
+		msg.channel.send(attachment);
+	}
+
+	if(cont.match(/^!reserve$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/reserve.png');
+		msg.channel.send(attachment);
+	}
+
+	if(cont.match(/^!woods$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/woods.png');
+		msg.channel.send(attachment);
+	}
+
+	if(cont.match(/^!shoreline$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/shoreline.png');
+		msg.channel.send(attachment);
+	}
+
+	if(cont.match(/^!labs$/)){
+		const attachment = new Discord.MessageAttachment('./src/tarkov/labs.png');
+		msg.channel.send(attachment);
 	}
 
 });
