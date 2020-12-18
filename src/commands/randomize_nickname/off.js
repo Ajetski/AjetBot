9220e274d325370bde309681b3d84039becb0c24
SamB133 <64711289+SamB133@users.noreply.github.com>
2020-12-17 21:05:14 -0500

    Name Randomizer
    Added a Discord nickname randomizer with a random time delay to change the nicknames. Added the commands: !on, !off, !addname, and !removename.

---
+++ src/commands/randomize_nickname/off.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+const regex = /^!off .*$/i;
+
+const func = (msg) => {
+    const fileName = path.join(__dirname, '../../../media/names.txt')
+    const people = JSON.parse(fs.readFileSync(fileName).toString());
+    let splitString = msg.content.split(' ');
+    let userID = splitString[1].substr(3, splitString[1].length - 4);
+    if (people.hasOwnProperty(userID)) {
+        people[userID].enabled = false;
+    }
+    fs.writeFileSync(fileName, JSON.stringify(people));
+}
+
+module.exports = {
+    regex,
+    func
+};
